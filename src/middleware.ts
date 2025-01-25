8d7b1faa4f2db4481ebe0776172e4372a589e301
Mohammad Ammar <ammaraslam7164@gmail.com>
2025-01-25 14:06:55 +0600

    middleware setted

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+
+
+export const middleware = async (request) => {
+
+    const token = await  cookies().get("next-auth.session-token")
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+
+    return NextResponse.next()
+
+}
+
+export const config = {
+
+
+    matcher: ["/cart", "/dashboard"]
+}
